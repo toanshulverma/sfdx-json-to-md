9c3fdd71a80f9bf97bece6917e53413b278a21e5
Anshul <toanshulverma@hotmail.com>
2024-01-09 17:23:46 -0600

    invoke utiilty via github actions

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const core = require('@actions/core')
 const exec = require('child_process').exec
-const fs = require('fs')
-const helper = require("./bin/sfdx-json-to-md")
 
 try {
-    install();
-} catch (error) {
-    core.setFailed(error.message)
-}
-
-function install(){
-    var download = 'npm install -g .'
-    exec( download, function(error, stdout, stderr){
+    let inputFile = core.getInput("InputFile");
+    let mode = core.getInput("Mode");
+    var command = 'node ./sfdx-json-to-md -i ' + inputFile + ' -m ' + mode;
+    exec( command, function(error, stdout, stderr){
         if(error) throw(stderr)
         core.info(stdout);
     });
+} catch (error) {
+    core.setFailed(error.message)
 }
